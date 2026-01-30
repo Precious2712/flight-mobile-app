e3c3ddc4abedfb99c7966b7a7fc35347266cdfcc
Precious2712 <oluwanifemimoonlight@gmail.com>
2026-01-30 10:44:03 -0800

    Error fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,16 @@
+import 'react-native-reanimated'
+
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
-import 'react-native-reanimated'
 import Toast from 'react-native-toast-message'
 
-import { useColorScheme } from '@/hooks/use-color-scheme'
-import { ProductProvider } from '@/context/useContext'
 import { DarkThemeApp, LightTheme } from '@/constants/navigation'
+import { ProductProvider } from '@/context/useContext'
+import { useColorScheme } from '@/hooks/use-color-scheme'
 
 export default function RootLayout() {
   const scheme = useColorScheme()
   const isDark = scheme === 'dark'
-
   const theme = isDark ? DarkThemeApp : LightTheme
 
   return (
@@ -31,7 +31,6 @@ export default function RootLayout() {
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
-
           <Stack.Screen name="auth/callback" options={{ headerShown: false }} />
         </Stack>
       </ProductProvider>
