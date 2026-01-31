a28ba8571256eddc36553a7c399b4e2d1f1b30e7
Precious2712 <oluwanifemimoonlight@gmail.com>
2026-01-31 10:35:46 -0800

    Updated env

--- lib/superbase.ts
+++ lib/superbase.ts
@@ -1,20 +1,3 @@
-// import 'react-native-url-polyfill'
-// import { createClient } from '@supabase/supabase-js'
-
-// const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL
-// const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
-
-// if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-//   throw new Error(
-//     'Missing Supabase environment variables. Check your .env file.'
-//   )
-// }
-
-// export const supabase = createClient(
-//   SUPABASE_URL,
-//   SUPABASE_ANON_KEY
-// )
-
 
 import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
